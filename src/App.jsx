7e3be16469b8150967f91d4746126d869f7a0faf
g7757 <gradling08@gmail.com>
2025-08-01 15:12:30 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Brooks Strategic Solutions</h1>
+      <p>Empowering your future, one solution at a time.</p>
+    </div>
+  );
+}
+
+export default App;
